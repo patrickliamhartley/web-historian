14f986bf0c4311da27d391e3d58b1636cbe100b8
patrickliamhartley <patrickliamhartley@gmail.com>
2016-08-17 20:04:03 -0700

    Somewhat implement workers

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,12 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var http = require('http');
+http.get('http://www.google.com/index.html', (res) => {
+  // consume response body
+  res.setEncoding('utf8');
+  res.on('data', function (data) {
+    console.log(data);
+  });
+}).on('error', (e) => {
+  console.log(`Got error: ${e.message}`);
+});
