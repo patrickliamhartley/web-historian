a0aa47495ff44ea10ae9f8e55c6f91e11849e669
patrickliamhartley <patrickliamhartley@gmail.com>
2016-08-18 12:31:06 -0700

    Make some progress

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,12 +1,24 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var fs = require('fs');
 var http = require('http');
-http.get('http://www.google.com/index.html', (res) => {
+var archive = require('../helpers/archive-helpers.js');
+var url = 'learn.shayhowe.com';
+http.get('http://' + url, (res) => {
   // consume response body
+  var page = '';
   res.setEncoding('utf8');
-  res.on('data', function (data) {
-    console.log(data);
+  res.on('data', function (chunk) {
+    page += chunk;
+  });
+  res.on('end', function () {
+    fs.writeFile(archive.paths.archivedSites + '/' + url + '.html', page, function (err) {
+      if (err) {
+        throw err;
+      }
+    });
   });
 }).on('error', (e) => {
   console.log(`Got error: ${e.message}`);
 });
+
